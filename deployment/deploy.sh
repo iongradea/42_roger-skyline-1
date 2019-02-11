545a4a972f28cfa87154d72354b5f1c3571a85bc
Ion GRADEA <igradea@e1r11p8.42.fr>
2019-02-11 18:18:43 +0100

    good dealgit add -A! wroking well

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -16,6 +16,11 @@ fi
 #
 
 USER=$1
+CRONTAB=/etc/crontab
+CRON_PATH=/etc/cron.d
+CRON_UPDA=update_pckg
+CRON_CH=ch_crontab
+CRON_FOLDER=./cron
 
 main() {
 
@@ -58,6 +63,36 @@ ch_err
 /etc/init.d/ssh restart
 ch_err
 ok_msg "ssh service on port 40 configured"
+
+## Step5 : configure cron
+#
+
+cp $CRON_FOLDER/$CRON_CH $CRON_PATH
+cp $CRON_FOLDER/$CRON_UPDA $CRON_PATH
+
+if grep -E "$CRON_PATH/$CRON_UPDA" $CRONTAB | grep -v "@reboot"; then
+	ok_msg "cron update pckg already present"
+else
+	echo "* 4	* * *	root	$CRON_PATH/$CRON_UPDA" >> $CRONTAB
+	ch_err
+	ok_msg "cron update pckg added"
+fi
+
+if grep -E "$CRON_PATH/$CRON_CH" $CRONTAB; then
+	ok_msg "cron check modifs already present in crontab file"
+else
+	echo "* 0	* * *	root	$CRON_PATH/$CRON_CH" >> $CRONTAB
+	ch_err
+	ok_msg "cron check modifs in crontab script added to crontab"
+fi
+
+if grep -E "@reboot.*$CRON_PATH/$CRON_UPDA" $CRONTAB; then
+	ok_msg "cron update pckg at reboot already present in crontab" 
+else
+	echo "@reboot root $CRON_PATH/$CRON_UPDA" >> $CRONTAB
+	ch_err
+	ok_msg "cron update pckg at reboot added"
+fi
 
 ## Step final : remove ip provided by 42 dhcp server / it breaks the connection
 #
